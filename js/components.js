2cbabae3b5b45107bc2dd3df8899beedc8fdd7d6
Pharallaxe <103119043+Pharallaxe@users.noreply.github.com>
2024-09-01 22:26:39 +0200

    Update components.js

--- js/components.js
+++ js/components.js
@@ -167,7 +167,7 @@ async function initializeContent(techno) {
     contentContainer.appendChild(homeButton);
     contentContainer.appendChild(colorBtn);
 
-    await fetch("../json/_data.json")
+    await fetch("https://pharallaxe.github.io/doc_public/json//json/_data.json")
         .then((response) => response.json())
         .then((data) => {
             const selectedData = data[techno];
