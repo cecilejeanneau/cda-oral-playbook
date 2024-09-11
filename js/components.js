0c3e3904130c8b0364878bdebf62278a84acdc74
Pharallaxe <103119043+Pharallaxe@users.noreply.github.com>
2024-09-11 11:18:05 +0200

    Suppression icone couleur.

--- js/components.js
+++ js/components.js
@@ -1,58 +1,37 @@
-function escapeHTML(html){const escapeMap={"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;","`":"&#96;",};return html.replace(/[&<>"'`]/g,(match)=>escapeMap[match])}
-class Accordion{constructor(containerId,jsonUrl){this.container=document.getElementById(containerId);this.jsonUrl=jsonUrl;this.data=null;this.commandCounter=0}
-async init(){await this.loadJSON();this.renderSections()}
-async loadJSON(){try{const response=await fetch(this.jsonUrl);this.data=await response.json()}catch(error){console.error("Erreur de chargement du JSON:",error)}}
-renderSections(){if(!this.data||!this.data.sections)return;this.data.sections.forEach((section,sectionIndex)=>{console.log(this.container)
-this.container.appendChild(this.createSection(section,sectionIndex))});this.addCopyHandlers()}
-createSection(section,sectionIndex){const sectionDiv=document.createElement("div");sectionDiv.classList.add("mb-4");sectionDiv.innerHTML=`
-            <div>
-                <h2 class="background accordion-header h2 bg-success p-2 text-white" id="headingSection${sectionIndex}" data-bs-toggle="collapse" data-bs-target="#innerAccordion${sectionIndex}" aria-expanded="false" aria-controls="innerAccordion${sectionIndex}">
-                    ${section.title}
-                </h2>
-                <div> 
-                    <div class="accordion rounded-bottom collapse" id="innerAccordion${sectionIndex}">
-                    </div>
-                </div>
-            </div>`;const innerAccordion=sectionDiv.querySelector(`#innerAccordion${sectionIndex}`);section.items.forEach((item,itemIndex)=>{innerAccordion.appendChild(this.createItem(item,sectionIndex,itemIndex))});return sectionDiv}
-createItem(item,sectionIndex,itemIndex){const itemDiv=document.createElement("div");itemDiv.classList.add("accordion-item");const itemId=`collapseItem${sectionIndex}${itemIndex}`;const itemHeader=`
-            <h2 class="accordion-header" id="headingItem${sectionIndex}${itemIndex}">
-                <button class="background accordion-button collapsed bg-success text-dark bg-opacity-50 border" type="button" data-bs-toggle="collapse" data-bs-target="#${itemId}" aria-expanded="false" aria-controls="${itemId}">
-                    ${item.title}
-                </button>
-            </h2>
-            <div id="${itemId}" class="accordion-collapse collapse" aria-labelledby="headingItem${sectionIndex}${itemIndex}" data-bs-parent="#innerAccordion${sectionIndex}">
-                <div class="background accordion-body bg-success p-2 text-dark bg-opacity-10 pb-3">
-                    ${this.createCommands(item.commands)}
-                </div>
-            </div>`;itemDiv.innerHTML=itemHeader;return itemDiv}
-createCommands(commands){this.commandCounter+=1;const counter=this.commandCounter;return commands.map((command,index)=>{let transformedCode=Array.isArray(command.code)?command.code.join("\n"):command.code;return `
-                    ${command.label ? this.formatLabels(command.label) : ""}
-                    ${command.code ? this.formatCode(command.code, counter, index) : ""}
-                `}).join("")}
-formatCode(code,counter,index){return `
-            <div class="code-block position-relative rounded" title="Cliquez pour copier" id="code-block-${counter}-${index}">
-                <pre style="background-color:inherit"><code>${escapeHTML(code)}</code></pre>
+function escapeHTML(e){let t={"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;","`":"&#96;"};return e.replace(/[&<>"'`]/g,e=>t[e])}class Accordion{constructor(e,t){this.container=document.getElementById(e),this.jsonUrl=t,this.data=null,this.commandCounter=0}async init(){await this.loadJSON(),this.renderSections()}async loadJSON(){try{let e=await fetch(this.jsonUrl);this.data=await e.json()}catch(t){console.error("Erreur de chargement du JSON:",t)}}renderSections(){this.data&&this.data.sections&&(this.data.sections.forEach((e,t)=>{console.log(this.container),this.container.appendChild(this.createSection(e,t))}),this.addCopyHandlers())}createSection(e,t){let a=document.createElement("div");a.classList.add("mb-4"),a.innerHTML=`
+    <div>
+        <h2 class="background accordion-header h2 bg-success p-2 text-white" id="headingSection${t}" data-bs-toggle="collapse" data-bs-target="#innerAccordion${t}" aria-expanded="false" aria-controls="innerAccordion${t}">
+            ${e.title}
+        </h2>
+        <div> 
+            <div class="accordion rounded-bottom collapse" id="innerAccordion${t}">
             </div>
-        `}
-formatLabels(label){const replaceQuotes=(text)=>{return text.replace(/\[strong\]/g,'<strong>').replace(/\[\/strong\]/g,'</strong>')};if(Array.isArray(label)){return label.map(item=>{const escapedItem=escapeHTML(item);const className=item.trim().startsWith("-")?"p-list":"";return `<p class="${className}">${replaceQuotes(escapedItem)}</p>`}).join("")}else{const escapedLabel=escapeHTML(label);return `<p>${replaceQuotes(escapedLabel)}</p>`}}
-addCopyHandlers(){document.querySelectorAll(".code-block").forEach((block)=>{block.addEventListener("click",()=>{const codeBlock=block.querySelector("code");const text=codeBlock.innerText||codeBlock.textContent;navigator.clipboard.writeText(text).then(()=>{block.classList.add("pressed");block.setAttribute("title","Cliquez pour copier");setTimeout(()=>{block.classList.remove("pressed")},200)}).catch((err)=>{console.error("Échec de la copie: ",err)})})})}}
-function createHomeButton(){const homeButtonContainer=document.createElement("div");homeButtonContainer.className="home-button-container";homeButtonContainer.innerHTML=`
-        <a href="../index.html" class="background button-common home-button">
-            <i class="bi bi-house-fill"></i>
-        </a>
-    `;return homeButtonContainer}
-function createContent(data){let img="";if(data.image!=="")img=`<img src="${data.image}" class="card-img-top" alt="${data.title}">`
-const contentDiv=document.createElement("div");contentDiv.className="card-body d-flex flex-column mt-5 mb-5";contentDiv.innerHTML=`
-        <h1 class="text-center mb-4">Documentation ${data.title}</h1>
-        ${img}
-        <p>${data.description}</p>
-        <p>${data.details}</p>
-        <p>${data.usage}</p>
-    `;return contentDiv}
-function createColorButton(){const colorDiv=document.createElement("div");colorDiv.className="color-changer-container";return colorDiv}
-function createContentAccordion(){const contentAccordion=document.createElement("div");contentAccordion.id="accordion-container";return contentAccordion}
-async function initializeContent(techno){const contentContainer=document.createElement("div");contentContainer.className="container my-5";const homeButton=createHomeButton();const colorBtn=createColorButton();contentContainer.appendChild(homeButton);contentContainer.appendChild(colorBtn);await fetch("../json/data.json").then((response)=>response.json()).then((data)=>{const selectedData=data[techno];console.log(selectedData)
-contentContainer.appendChild(createContent(selectedData));contentContainer.appendChild(createContentAccordion());document.querySelector("body").appendChild(contentContainer)}).catch((error)=>console.error("Erreur lors du chargement des données:",error));initializeAccess()
-const accordion=new Accordion("accordion-container",`../json/${techno}.json`);accordion.init()}
-function applyBackgroundColor(){const currentColor=localStorage.getItem("backgroundColor")||"bg-primary";const elements=document.querySelectorAll(".background");elements.forEach((element)=>{element.classList.forEach((className)=>{if(className.startsWith("bg-")&&!className.startsWith("bg-opacity")){element.classList.remove(className)}});element.classList.add(currentColor)})}
-function initializeAccess(){applyBackgroundColor();const container=document.querySelector(".color-changer-container");console.log(container);const colorChangerBtn=document.createElement("button");colorChangerBtn.className="button-common color-changer-btn";colorChangerBtn.title="Changer la couleur";colorChangerBtn.innerHTML="🎨";const colorPickerBar=document.createElement("div");colorPickerBar.className="color-picker-bar";const colors=[{className:"bg-primary",colorName:"bg-primary"},{className:"bg-danger",colorName:"bg-danger"},{className:"bg-warning",colorName:"bg-warning"},{className:"bg-info",colorName:"bg-info"},{className:"bg-dark",colorName:"bg-dark"},{className:"bg-success",colorName:"bg-success"},];colors.forEach((color)=>{const colorBtn=document.createElement("button");colorBtn.className=`color-btn ${color.className}`;colorBtn.setAttribute("data-color",color.colorName);colorPickerBar.appendChild(colorBtn)});container.appendChild(colorChangerBtn);container.appendChild(colorPickerBar);const currentColor=localStorage.getItem("backgroundColor")||"bg-primary";const elements=document.querySelectorAll(".background");elements.forEach((element)=>{element.classList.add(currentColor)});colorChangerBtn.addEventListener("click",()=>{if(colorPickerBar.style.display==="none"||colorPickerBar.style.display===""){colorPickerBar.style.display="block"}else{colorPickerBar.style.display="none"}});colorPickerBar.addEventListener("click",(event)=>{if(event.target.classList.contains("color-btn")){const newColorClass=event.target.getAttribute("data-color");const elements=document.querySelectorAll(".background");elements.forEach((element)=>{element.classList.forEach((className)=>{if(className.startsWith("bg-")&&!className.startsWith("bg-opacity")){element.classList.remove(className)}});element.classList.add(newColorClass)});colorPickerBar.style.display="none";localStorage.setItem("backgroundColor",newColorClass)}})}
+        </div>
+    </div>`;let o=a.querySelector(`#innerAccordion${t}`);return e.items.forEach((e,a)=>{o.appendChild(this.createItem(e,t,a))}),a}createItem(e,t,a){let o=document.createElement("div");o.classList.add("accordion-item");let n=`collapseItem${t}${a}`,r=`
+    <h2 class="accordion-header" id="headingItem${t}${a}">
+        <button class="background accordion-button collapsed bg-success text-dark bg-opacity-50 border" type="button" data-bs-toggle="collapse" data-bs-target="#${n}" aria-expanded="false" aria-controls="${n}">
+            ${e.title}
+        </button>
+    </h2>
+    <div id="${n}" class="accordion-collapse collapse" aria-labelledby="headingItem${t}${a}" data-bs-parent="#innerAccordion${t}">
+        <div class="background accordion-body bg-success p-2 text-dark bg-opacity-10 pb-3">
+            ${this.createCommands(e.commands)}
+        </div>
+    </div>`;return o.innerHTML=r,o}createCommands(e){this.commandCounter+=1;let t=this.commandCounter;return e.map((e,a)=>(Array.isArray(e.code)?e.code.join("\n"):e.code,`
+            ${e.label?this.formatLabels(e.label):""}
+            ${e.code?this.formatCode(e.code,t,a):""}
+        `)).join("")}formatCode(e,t,a){return`
+    <div class="code-block position-relative rounded" title="Cliquez pour copier" id="code-block-${t}-${a}">
+        <pre style="background-color:inherit"><code>${escapeHTML(e)}</code></pre>
+    </div>
+`}formatLabels(e){let t=e=>e.replace(/\[strong\]/g,"<strong>").replace(/\[\/strong\]/g,"</strong>");if(Array.isArray(e))return e.map(e=>{let a=escapeHTML(e),o=e.trim().startsWith("-")?"p-list":"";return`<p class="${o}">${t(a)}</p>`}).join("");{let a=escapeHTML(e);return`<p>${t(a)}</p>`}}addCopyHandlers(){document.querySelectorAll(".code-block").forEach(e=>{e.addEventListener("click",()=>{let t=e.querySelector("code"),a=t.innerText||t.textContent;navigator.clipboard.writeText(a).then(()=>{e.classList.add("pressed"),e.setAttribute("title","Cliquez pour copier"),setTimeout(()=>{e.classList.remove("pressed")},200)}).catch(e=>{console.error("\xc9chec de la copie: ",e)})})})}}function createHomeButton(){let e=document.createElement("div");return e.className="home-button-container",e.innerHTML=`
+<a href="../index.html" class="background button-common home-button">
+    <i class="bi bi-house-fill"></i>
+</a>
+`,e}function createContent(e){let t="";""!==e.image&&(t=`<img src="${e.image}" class="card-img-top" alt="${e.title}">`);let a=document.createElement("div");return a.className="card-body d-flex flex-column mt-5 mb-5",a.innerHTML=`
+<h1 class="text-center mb-4">Documentation ${e.title}</h1>
+${t}
+<p>${e.description}</p>
+<p>${e.details}</p>
+<p>${e.usage}</p>
+`,a}function createColorButton(){let e=document.createElement("div");return e.className="color-changer-container",e}function createContentAccordion(){let e=document.createElement("div");return e.id="accordion-container",e}async function initializeContent(e){let t=document.createElement("div");t.className="container my-5";let a=createHomeButton(),o=createColorButton();t.appendChild(a),t.appendChild(o),await fetch("../json/data.json").then(e=>e.json()).then(a=>{let o=a[e];console.log(o),t.appendChild(createContent(o)),t.appendChild(createContentAccordion()),document.querySelector("body").appendChild(t)}).catch(e=>console.error("Erreur lors du chargement des donn\xe9es:",e));let n=new Accordion("accordion-container",`../json/${e}.json`);n.init()}
